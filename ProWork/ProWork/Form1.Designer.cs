e9bba17280c498afa4bfd1fcc31bd1833d0952ff
CrisRPia <102370984+CrisRPia@users.noreply.github.com>
2022-07-05 11:08:10 -0300

    Add files via upload
    Primera versión de Prowork en base al diseño de Augusto, conteniendo la base del login.

---
+++ ProWork/ProWork/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ProWork
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbContra = new System.Windows.Forms.TextBox();
+            this.txbCContra = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tmrSubrayado = new System.Windows.Forms.Timer(this.components);
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.pnlForeground = new System.Windows.Forms.Panel();
+            this.mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnContra = new System.Windows.Forms.Button();
+            this.btnCContra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.txbNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbNombre.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbNombre.Location = new System.Drawing.Point(450, 242);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.PlaceholderText = "Nombre";
+            this.txbNombre.Size = new System.Drawing.Size(333, 36);
+            this.txbNombre.TabIndex = 0;
+            this.txbNombre.Enter += new System.EventHandler(this.txb_Enter);
+            this.txbNombre.Leave += new System.EventHandler(this.txb_Leave);
+            // 
+            // txbContra
+            // 
+            this.txbContra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.txbContra.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbContra.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbContra.Location = new System.Drawing.Point(450, 294);
+            this.txbContra.Name = "txbContra";
+            this.txbContra.PlaceholderText = "Contraseña";
+            this.txbContra.Size = new System.Drawing.Size(291, 36);
+            this.txbContra.TabIndex = 2;
+            this.txbContra.UseSystemPasswordChar = true;
+            this.txbContra.Enter += new System.EventHandler(this.txb_Enter);
+            // 
+            // txbCContra
+            // 
+            this.txbCContra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.txbCContra.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbCContra.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbCContra.Location = new System.Drawing.Point(450, 346);
+            this.txbCContra.Name = "txbCContra";
+            this.txbCContra.PlaceholderText = "Confirmar contraseña";
+            this.txbCContra.Size = new System.Drawing.Size(291, 36);
+            this.txbCContra.TabIndex = 4;
+            this.txbCContra.UseSystemPasswordChar = true;
+            this.txbCContra.Enter += new System.EventHandler(this.txb_Enter);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(450, 433);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(333, 55);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Crear";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tmrSubrayado
+            // 
+            this.tmrSubrayado.Interval = 16;
+            this.tmrSubrayado.Tick += new System.EventHandler(this.tmrSubrayado_Tick);
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.btnSwap.FlatAppearance.BorderSize = 0;
+            this.btnSwap.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSwap.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(75)))));
+            this.btnSwap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSwap.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSwap.Location = new System.Drawing.Point(450, 494);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(234, 29);
+            this.btnSwap.TabIndex = 7;
+            this.btnSwap.Text = "¿Tienes una cuenta? Inicia sesión";
+            this.btnSwap.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSwap.UseVisualStyleBackColor = false;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // pnlForeground
+            // 
+            this.pnlForeground.BackColor = System.Drawing.Color.Black;
+            this.pnlForeground.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlForeground.Location = new System.Drawing.Point(0, 0);
+            this.pnlForeground.Name = "pnlForeground";
+            this.pnlForeground.Size = new System.Drawing.Size(417, 603);
+            this.pnlForeground.TabIndex = 8;
+            this.pnlForeground.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlForeground_Paint);
+            // 
+            // mySqlCommand1
+            // 
+            this.mySqlCommand1.CacheAge = 0;
+            this.mySqlCommand1.Connection = null;
+            this.mySqlCommand1.EnableCaching = false;
+            this.mySqlCommand1.Transaction = null;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProWork.Properties.Resources.Icono_Registro;
+            this.pictureBox1.Location = new System.Drawing.Point(559, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnContra
+            // 
+            this.btnContra.FlatAppearance.BorderSize = 0;
+            this.btnContra.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(75)))));
+            this.btnContra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContra.Image = global::ProWork.Properties.Resources.ojitosi321;
+            this.btnContra.Location = new System.Drawing.Point(746, 294);
+            this.btnContra.Name = "btnContra";
+            this.btnContra.Size = new System.Drawing.Size(37, 36);
+            this.btnContra.TabIndex = 10;
+            this.btnContra.UseVisualStyleBackColor = true;
+            this.btnContra.Click += new System.EventHandler(this.pbxOContra_Click);
+            this.btnContra.Paint += new System.Windows.Forms.PaintEventHandler(this.pbxOContra_Paint);
+            // 
+            // btnCContra
+            // 
+            this.btnCContra.FlatAppearance.BorderSize = 0;
+            this.btnCContra.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(75)))));
+            this.btnCContra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCContra.Image = global::ProWork.Properties.Resources.ojitosi321;
+            this.btnCContra.Location = new System.Drawing.Point(746, 346);
+            this.btnCContra.Name = "btnCContra";
+            this.btnCContra.Size = new System.Drawing.Size(37, 36);
+            this.btnCContra.TabIndex = 11;
+            this.btnCContra.UseVisualStyleBackColor = true;
+            this.btnCContra.Click += new System.EventHandler(this.btnCContra_Click);
+            this.btnCContra.Paint += new System.Windows.Forms.PaintEventHandler(this.btnCContra_Paint);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(821, 603);
+            this.Controls.Add(this.btnCContra);
+            this.Controls.Add(this.btnContra);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.pnlForeground);
+            this.Controls.Add(this.txbContra);
+            this.Controls.Add(this.txbCContra);
+            this.Controls.Add(this.btnLogin);
+            this.DoubleBuffered = true;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Usuarios";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmLogin_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txbNombre;
+        private TextBox txbContra;
+        private TextBox txbCContra;
+        private Button btnLogin;
+        private System.Windows.Forms.Timer tmrSubrayado;
+        private Button btnSwap;
+        private Panel pnlForeground;
+        private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
+        private PictureBox pictureBox1;
+        private Button btnContra;
+        private Button btnCContra;
+    }
+}
